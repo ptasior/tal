24a5827a01eada37ef528b538f0f8a8b790e3a20
Maciek <ptasior3@gazeta.pl>
2018-04-19 20:41:29 +1000

    Android project

---
+++ lib/lua/android/Application.mk
@@ -0,0 +1,8 @@
+APP_ABI := armeabi-v7a
+APP_PLATFORM := android-25
+APP_ATL := stlport_static
+APP_BUILD_SCRIPT := Android.mk
+# APP_STL := c++_static
+APP_STL := c++_shared
+# LOCAL_CFLAGS := -DLUA_USE_POSIX
+
